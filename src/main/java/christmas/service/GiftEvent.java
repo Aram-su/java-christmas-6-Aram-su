6eca922437ad6c996343688fc29b2f3c0037e300
Seokhyun Lee <lseokhyun99@gmail.com>
2023-11-14 18:08:59 +0900

    fix(GiftEvent) : 증정품의 데이터 타입을 수정

--- src/main/java/christmas/service/GiftEvent.java
+++ src/main/java/christmas/service/GiftEvent.java
@@ -1,16 +1,21 @@
 package christmas.service;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class GiftEvent {
 
-    public static final String ONE_CHAMPAGNE = "샴페인 1개";
-    public static final String NONE = "없음";
+    public static final String CHAMPAGNE = "샴페인";
     public static final int CHAMPAGNE_THRESHOLD = 120000;
+    public static final int ONE = 1;
 
-    public static String determineGift(int totalOrderAmount) {
+    public static Map<String, Integer> determineGift(int totalOrderAmount) {
+        Map<String, Integer> giftAndQuantity = new HashMap<>();
+
         if (totalOrderAmount >= CHAMPAGNE_THRESHOLD) {
-            return ONE_CHAMPAGNE;
+            giftAndQuantity.put(CHAMPAGNE, ONE);
         }
-        return NONE;
+        return giftAndQuantity;
     }
 
 }
